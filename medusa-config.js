84ada5ffa415efe074bf7afb32539790f18f010d
anaghastaff <anaghastaff123@gmail.com>
2024-06-05 11:35:24 +0530

    testing cloudinary plugin

--- medusa-config.js
+++ medusa-config.js
@@ -81,6 +81,15 @@ const plugins = [
       authWebhookId: process.env.PAYPAL_AUTH_WEBHOOK_ID,
     },
   },
+  {
+    resolve: `medusa-file-cloudinary`,
+    options: {
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+        secure: process.env.CLOUDINARY_SECURE,
+    },
+},
 ];
 
 // const modules = {
