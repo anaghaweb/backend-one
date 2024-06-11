0b30a90833c90d42e74d32628223faeeb7e7ff2c
anaghastaff <anaghastaff123@gmail.com>
2024-06-11 17:15:08 +0530

    added a store api endpoint

--- src/api/store/products/[id]/reviews/route.ts
+++ src/api/store/products/[id]/reviews/route.ts
@@ -4,19 +4,20 @@ import ProductReviewService from "../../../../../services/product-review";
 export default async function GET(req: MedusaRequest, res: MedusaResponse) {
   try {
     const productReviewService: ProductReviewService = req.scope.resolve("productReviewService");
-    const productReviews = await productReviewService.getProductReviews(req.params.id);
-    if(!productReviews){
+    const product_reviews = await productReviewService.getProductReviews(req.params.id);
+    if(!product_reviews){
         return res.json({
             status: 'error',      
             message: 'No rpoduct reviews for this product yet!',
           });
     }
-    
+    else{
     return res.json({
       status: 'success',
-      data: productReviews,
+      data: product_reviews,
       message: 'Product review retrieved successfully.',
     });
+}
   } catch (error) {
     return res.status(500).json({
       status: 'error',
